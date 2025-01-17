6c8253219eaff052d27f2e01fdbff0ebcede9991
misterlister <haydenglister@gmail.com>
2025-01-16 16:29:54 -0800

    Implemented powerups

--- player.py
+++ player.py
@@ -5,9 +5,14 @@ from constants import (
     PLAYER_TURN_SPEED,
     PLAYER_SPEED,
     PLAYER_COLOUR,
-    PLAYER_SHOT_BASE_SPEED
+    PLAYER_SHOT_BASE_SPEED,
+    POWERUP_SPEEDSHOT_FACTOR,
+    POWERUP_DURATION,
+    POWERUP_MULTISHOT_ANGLE,
+    POWERUP_SHIELD_COLOUR
 )
 from laser import Laser
+from powerup import Powerup, PowerupType
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -15,6 +20,12 @@ class Player(CircleShape):
         self.rotation = 0
         self.shot_timer = 0
         self.shot_speed = PLAYER_SHOT_BASE_SPEED
+        self.pierce_timer = 0
+        self.multishot_timer = 0
+        self.multishot_stacks = 0
+        self.speed_shot_timer = 0
+        self.colour = PLAYER_COLOUR
+        self.shield = False
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,7 +36,7 @@ class Player(CircleShape):
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, PLAYER_COLOUR, self.triangle(), width=2)
+        pygame.draw.polygon(screen, self.colour, self.triangle(), width=2)
     
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
@@ -34,6 +45,7 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         self.shot_timer -= dt
+        self.update_powerups(dt)
         
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
@@ -52,7 +64,66 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
         
     def shoot(self):
-        shot = Laser(self.position.x, self.position.y)
-        shot.fire(self.rotation)
+        # Fire the main shot
+        main_shot = Laser(self.position.x, self.position.y)
+        main_shot.fire(self.rotation)
+
+        # Fire additional shots for each multishot stack
+        for stack in range(1, self.multishot_stacks + 1):
+            # Calculate the angles for each stack
+            left_angle = self.rotation - (stack * POWERUP_MULTISHOT_ANGLE)
+            right_angle = self.rotation + (stack * POWERUP_MULTISHOT_ANGLE)
+
+            # Fire the left shot
+            left_shot = Laser(self.position.x, self.position.y)
+            left_shot.fire(left_angle)
+
+            # Fire the right shot
+            right_shot = Laser(self.position.x, self.position.y)
+            right_shot.fire(right_angle)
+
+        # Reset the shot timer
         self.shot_timer = self.shot_speed
         
+    def update_powerups(self, dt):
+        if self.pierce_timer > 0:
+            self.pierce_timer -= dt
+        if self.multishot_timer > 0:
+            self.multishot_timer -= dt
+            if self.multishot_timer <= 0:
+                self.multishot_stacks = 0
+        if self.speed_shot_timer > 0:
+            self.speed_shot_timer -= dt
+            if self.speed_shot_timer <= 0:
+                self.shot_speed = PLAYER_SHOT_BASE_SPEED
+        
+    def get_powerup(self, powerup: Powerup):
+        match (powerup.type):
+            case PowerupType.SPEEDSHOT:
+                self.shot_speed *= POWERUP_SPEEDSHOT_FACTOR
+                self.speed_shot_timer = POWERUP_DURATION
+            case PowerupType.PIERCE:
+                self.pierce_timer = POWERUP_DURATION
+            case PowerupType.MULTISHOT:
+                self.multishot_timer = POWERUP_DURATION
+                self.multishot_stacks += 1
+            case PowerupType.SHIELD:
+                self.get_shield()
+        powerup.kill()
+        
+    def multishot_powerup(self):
+        return self.multishot_timer > 0
+    
+    def pierce_powerup(self):
+        return self.pierce_timer > 0
+    
+    def shielded(self):
+        return self.shield
+    
+    def get_shield(self):
+        self.shield = True
+        self.colour = POWERUP_SHIELD_COLOUR
+        
+    def lose_shield(self):
+        self.shield = False
+        self.colour = PLAYER_COLOUR
